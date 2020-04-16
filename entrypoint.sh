71cb275aedeba2be5b65666133d211e9ddb063f6
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 17:33:46 +0200

    Manual debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 # Terminate upon errors
 set -e
 # Run cmake
+pwd
+ls
 cmake -S ${INPUT_SCANDIR} -B ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
