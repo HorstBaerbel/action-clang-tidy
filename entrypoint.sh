55f40003d2764ef9573311c1970f6d97a4c440cb
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 22:53:00 +0200

    Try relative paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,14 @@ set -e
 #cd ${INPUT_BUILDDIR}
 #cmake ${INPUT_SCANDIR}
 # does not work either -> CMake Error: The source directory "/github/workspace/build" does not appear to contain CMakeLists.txt.
-pwd
-echo $GITHUB_WORKSPACE
-echo ${INPUT_SCANDIR}
-echo ${INPUT_BUILDDIR}
-echo ${INPUT_EXCLUDEDIRS}
-echo ${INPUT_EXTENSIONS}
+#pwd
+#echo $GITHUB_WORKSPACE
+#echo ${INPUT_SCANDIR}
+#echo ${INPUT_BUILDDIR}
+#echo ${INPUT_EXCLUDEDIRS}
+#echo ${INPUT_EXTENSIONS}
 mkdir ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
 cd ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
-cmake ${INPUT_SCANDIR}
-cd ..
-ls
+cmake ..
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
