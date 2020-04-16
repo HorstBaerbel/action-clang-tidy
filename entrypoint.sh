2877ac0b150f9d4954539131fcc6954ac315cf61
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 17:20:57 +0200

    Fully specify build dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 # Terminate upon errors
 set -e
 # Run cmake
-mkdir ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
-cmake -S ${INPUT_SCANDIR} -B ${INPUT_BUILDDIR}
+cmake -S ${INPUT_SCANDIR} -B ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
