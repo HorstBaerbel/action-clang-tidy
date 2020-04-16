e38b423e630175d54f6fed1a7667f936f338f253
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 18:28:52 +0200

    Not sure what is wrong

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 
 # Terminate upon errors
 set -e
-# Run cmake
-mkdir ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
-pwd
-ls
-cmake -S ${INPUT_SCANDIR} -B ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+# Run cmake to configure
+mkdir ${INPUT_BUILDDIR}
+cd ${INPUT_BUILDDIR}
+cmake ${INPUT_SCANDIR}
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
