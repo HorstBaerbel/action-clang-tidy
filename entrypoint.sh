c54bb6046de313152fc957161facda39603e2a33
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 20:56:11 +0200

    Debug what's wrong

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,11 @@ set -e
 # does not work either -> CMake Error: The source directory "/github/workspace/build" does not appear to contain CMakeLists.txt.
 pwd
 ls
-cmake -S ${INPUT_SCANDIR} -B ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+echo $GITHUB_WORKSPACE
+echo ${INPUT_SCANDIR}
+echo ${INPUT_BUILDDIR}
+echo ${INPUT_EXCLUDEDIRS}
+echo ${INPUT_EXTENSIONS}
+cmake --debug-output -Wdev -S ${INPUT_SCANDIR} -B ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
