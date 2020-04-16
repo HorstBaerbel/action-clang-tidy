3ad0098b010372bb1c229b9fe1caf20fa821ccf2
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 21:03:46 +0200

    installed cmake has no -S -B options

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,15 @@ set -e
 #cmake ${INPUT_SCANDIR}
 # does not work either -> CMake Error: The source directory "/github/workspace/build" does not appear to contain CMakeLists.txt.
 pwd
-ls
 echo $GITHUB_WORKSPACE
 echo ${INPUT_SCANDIR}
 echo ${INPUT_BUILDDIR}
 echo ${INPUT_EXCLUDEDIRS}
 echo ${INPUT_EXTENSIONS}
-cmake --debug-output -Wdev -S ${INPUT_SCANDIR} -B ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+mkdir ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+cd ${INPUT_SCANDIR}/${INPUT_BUILDDIR}
+cmake ${INPUT_SCANDIR}
+cd ..
+ls
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
