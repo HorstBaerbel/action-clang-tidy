fa89589a44b740e98d2ca2fa76017e8768dde81f
Bim Overbohm <bim.overbohm@googlemail.com>
2020-04-16 15:59:39 +0200

    Run cmake before clang-tidy to create build dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 # Terminate upon errors
 set -e
+# Run cmake
+cmake -S ${INPUT_SCANDIR} -B ${INPUT_BUILDDIR}
 # Run tidy script
 python3 /run-clang-tidy.py ${INPUT_SCANDIR} ${INPUT_BUILDDIR} ${INPUT_EXCLUDEDIRS} ${INPUT_EXTENSIONS}
